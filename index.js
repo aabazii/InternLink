0b8a5c7bc8cfe123076f10ff23467ec49a993068
aabazii <147416198+aabazii@users.noreply.github.com>
2025-01-14 14:45:59 +0200

    index.js

--- index.js
+++ index.js
@@ -3,6 +3,51 @@ const path = require('path');
 const redditData = require('./data.json');
 const port = 8080;
 const app = express();
+const mongoose = require("mongoose");
+
+
+// Middleware to parse JSON requests
+app.use(express.json());
+
+// Connect to MongoDB (Note: We'll set up MongoDB later)
+const dbURI = "mongodb://localhost:27017/interships";
+mongoose
+  .connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.error("Could not connect to MongoDB", err));
+
+  const intershipsSchema = new mongoose.Schema({
+    name: {
+        type:String,
+        required: true
+    },
+    details:{
+        type:String,
+        required: true
+    },
+    internType:{
+        type:String,
+        required: true
+    },
+    payment:{
+      type:String,
+      required: true
+    },
+    deadline:{
+      type:Date,
+      required: true
+    },
+    location:{
+      type:String,
+      required: true
+    }
+
+    
+
+    
+})
+
+const internships = mongoose.model('interships', intershipsSchema);
 
 app.set("view engine", "ejs");
 
@@ -27,3 +72,5 @@ app.get('/r/:subreddit', (req, res) => {
 app.listen(port, () => {
   console.log(`Server is running on port: ` + port);
 });
+
+
